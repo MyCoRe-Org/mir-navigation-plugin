91999aff6e7a7b249248a76ccb63e2d653f3edeb
Paul Rochowski <Paul.Rochowski@uni-due.de>
2019-01-24 12:45:50 +0100

    # get mycoreId from metadata

--- src/main/resources/META-INF/resources/js/mir-navigation-plugin/mir-navigation.js
+++ src/main/resources/META-INF/resources/js/mir-navigation-plugin/mir-navigation.js
@@ -1,6 +1,34 @@
 $(document).ready(function () {
+    /*
+     * get the root navigation document
+     */
+    let currentUrl = window.location.href;
+
+    let currentMyCoreObjectId = getCurrentMyCoreObjectId(currentUrl);
+
+    function getCurrentMyCoreObjectId(currentUrl) {
+
+        requestXSL(currentUrl).then((data) => {
+            var mycoreObjectId = $('mycoreobject', data).attr('ID');
+
+            if (!mycoreObjectId) {
+
+                var currentUrlParts = currentUrl.split('/');
+
+                currentUrl = currentUrl.replace(currentUrlParts[currentUrlParts.length - 1], '');
+
+                getCurrentMyCoreObjectId(currentUrl);
+            }
+        });
+    }
 
 
+    function requestXSL(currentUrl) {
 
-    console.log('mir-navigation.js: Script was successfully loaded.');
+        return $.ajax({
+            url: currentUrl + "?XSL.Style=xml",
+            dataType: "xml",
+            type: "GET",
+        });
+    }
 });
